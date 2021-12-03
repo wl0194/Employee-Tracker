9ef334b43c25e7813bbc38cf13ea2c5ae5683513
Amy Hwang <728amyh@gmail.com>
2021-12-03 12:53:15 -0500

    syntax error

--- employee.js
+++ employee.js
@@ -29,48 +29,28 @@ function menu() {
           "Add Department",
           "Quit",
         ]
-      },
-      {
-        type: "input",
-        name: "firstname",
-        message: "What is the employee's first name?"
-      },
-      // {
-      //   type: "input",
-      //   name: "lastname",
-      //   message: "What is the employee's last name?"
-      // },
-      // {
-      //   type: "list",
-      //   name: "role",
-      //   message: "What is the employee's role?",
-      //   choices: [
-      //     "Sales Lead",
-      //     "Salesperson",
-      //     "Lead Enginner",
-      //     "Software Engineer",
-      //     "Account Manager",
-      //     "Accountant",
-      //     "Legal Team Lead",
-      //     "Lawyer"
-      //   ]
-      // }
-
-    ])
-    .then((response) => {
-      console.log(response.choice)
-      if (response.choice === "View All Departments") {
-        getAllDepartments()
-      } else if ("View All Roles" === response.choice) {
-        getAllRoles()
-      } else if ("View All Employees" === response.name) {
-        getAllEmployees()
-      }  else if ("Add Role" === response.role) {
-        getAddRole()
       }
-    });
-};
-
+      .prompt([
+        {
+          type: "input",
+          message: "What is the name of the department?",
+          name: "department",
+        }
+        .then((response) => {
+            console.log(response.choice)
+            if (response.choice === "View All Departments") {
+              getAllDepartments()
+            } else if ("View All Roles" === response.choice) {
+              getAllRoles()
+            } else if ("View All Employees" === response.choice) {
+              getAllEmployees()
+            } else if ("Add Department" === response.department) {
+              console.log("Added to the database");
+              getAddDepartment()
+            }
+          });
+       
+        
 function getAllDepartments() {
   db.query("SELECT * FROM department", (err, results) => {
     if (err) {
@@ -92,7 +72,7 @@ function getAllRoles() {
   })
 }
 function getAllEmployees() {
-  db.query("SELECT * FROM employee(firstname,lastname)", (err, results) => {
+  db.query("SELECT * FROM employee", (err, results) => {
     if (err) {
       console.table(err);
     } else {
@@ -101,6 +81,7 @@ function getAllEmployees() {
     }
   })
 }
+
 // function getAddEmployee() {
 //   db.query("INSERT INTO employee(firstname,lastname)", (err, results) => {
 //     if (err) {
